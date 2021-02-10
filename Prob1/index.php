a2925733093876ac07dd82a0bc431c31fe82d4ee
Charles Reyes <charleskylereyes16@gmail.com>
2021-02-11 00:07:50 +0800

    Finished functionality of index.php

--- Prob1/index.php
+++ Prob1/index.php
@@ -2,9 +2,52 @@
 	<head>
 		<title>
 		</title>
-		<link rel='stylesheet' href='style.css'
+		<link rel='stylesheet' href='style.css'>
 	</head>
 	<body>
+		<?php
+		$names = array(
+			'james'  , 'mary', 
+			'bradley', 'charlotte', 
+			'johnny' , 'kayla',
+			'john'   , 'patricia',
+			'robert' , 'jennifer',
+			'michael', 'linda',
+			'william', 'elizabeth',
+			'david'  , 'barbara',
+			'richard', 'susan',
+			'joseph' , 'jessica')
+		?>
+		<table>
+			<tr>
+				<th colspan='6'>List of names</th>
+			</tr>
+			<tr>
+				<td>Name</td>
+				<td>Number of Character</td>
+				<td>Uppercase First Character</td>
+				<td>Replace Vowels with @</td>
+				<td>Check Position of Chracter 'a'</td>
+				<td>Reverse Name</td>
+			</tr>
+			<?php
+			
+			for ($i = 0; $i < count($names); $i++) {
+				echo "<tr>";
+				echo "<td>$names[$i]</td>";
+				$x = strlen($names[$i]);
+				echo "<td>$x</td>";
+				echo "<td>".ucfirst($names[$i])."</td>";
+				echo "<td>".str_replace('a', '@', $names[$i])."</td>";
+				echo "<td>".(int)strpos($names[$i], 'a')."</td>";
+				echo "<td>".strrev($names[$i])."</td>";
+				echo "</tr>";
+			}
+
+			?>
+		</table>
+
+
 	</body>
 
 </html>
